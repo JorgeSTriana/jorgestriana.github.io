6a0e7e80c53b4e5924bfc9be3f3d7829e49be6ae
JorgeSTriana <jorgetriana1951@gmail.com>
2022-08-29 16:32:33 -0500

    actualizacion de datos

--- Portafolio/index.php
+++ Portafolio/index.php
@@ -164,6 +164,13 @@
               </div>
             </div>
 
+            <div class="progress">
+              <span class="skill">React <i class="val">70%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
           </div>
 
           <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
@@ -186,6 +193,13 @@
               <span class="skill">java <i class="val">50%</i></span>
               <div class="progress-bar-wrap">
                 <div class="progress-bar" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">Node.js <i class="val">60%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
               </div>
             </div>
 
